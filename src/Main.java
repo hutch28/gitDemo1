dbf1103e929d13963d75e7fb571009d29d05286f
Jon Hutchins <HutchinsJO@gcc.edu>
2022-02-25 13:00:32 -0500

    restore

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello From 1");
-        System.out.println("Hello From2 really");
-        System.out.println("one more line");
-
-    }
-
-    public static void doSOmething(){
-        // todo
     }
 
 
