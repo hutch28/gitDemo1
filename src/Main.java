17c32c51e6ae451e0a50d3d2369e02254c1a8074
Jon Hutchins <HutchinsJO@gcc.edu>
2022-02-25 15:33:33 -0500

    i win

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello From 1 dont add blah");
 
-        System.out.println("Hello From 1 lave blah alone");
-
     }
 
 
